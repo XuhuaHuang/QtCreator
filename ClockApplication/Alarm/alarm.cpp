501895cbf5020a587dae66a17c490837cdf87481
Huang <xuhuahuang0412@gmail.com>
2021-03-25 18:44:13 -0400

    Updated layout

--- ClockApplication/Alarm/alarm.cpp
+++ ClockApplication/Alarm/alarm.cpp
@@ -1,8 +1,58 @@
 #include "alarm.h"
 
 Alarm::Alarm(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      hour(0),
+      minute(0),
+      snoozed(false),
+      activated(false)
 {
+    // initialize layout
+    setWindowTitle("Alarm");
+    // change the background color to dark grey
+    setStyleSheet("background-color:#848484;");
+    // timeHbox = new QHBoxLayout();
+    volumeHbox = new QHBoxLayout();
+    btnHbox = new QHBoxLayout();
+    vbox = new QVBoxLayout();
+
+    // button, spin boxes and label
+    separater = new QLabel(":");
+    status = new QLabel("Alarm status");
+    setVolume = new QLabel("Set Volume");
+    setAlarmbtn = new QPushButton("Set Alarm");
+    snoozebtn = new QPushButton("Snooze Alarm");
+    stopbtn = new QPushButton("Stop Alarm");
+
+    volumebar = new QSlider(Qt::Horizontal);
+    volumebar->setMinimum(0);
+    volumebar->setMaximum(100);
+
+    hourBox = new QSpinBox();
+    hourBox->setMinimum(0);
+    hourBox->setMaximum(23);
+
+    minuteBox = new QSpinBox();
+    minuteBox->setMinimum(0);
+    minuteBox->setMaximum(59);
+
+    // add objects to layout and set layout
+    timeHbox->addWidget(hourBox);
+    timeHbox->addWidget(separater);
+    timeHbox->addWidget(minuteBox);
+    timeHbox->addWidget(setAlarmbtn);
+
+    volumeHbox->addWidget(setVolume);
+    volumeHbox->addWidget(volumebar);
+
+    btnHbox->addWidget(snoozebtn);
+    btnHbox->addWidget(stopbtn);
+
+    vbox->addLayout(timeHbox);
+    vbox->addLayout(volumeHbox);
+    vbox->addWidget(status);
+    vbox->addLayout(btnHbox);
+    setLayout(vbox);
 }
 
 Alarm::~Alarm()
--- ClockApplication/Alarm/alarm.h
+++ ClockApplication/Alarm/alarm.h
@@ -1,7 +1,14 @@
 #ifndef ALARM_H
 #define ALARM_H
 
+#include <QDebug>
 #include <QWidget>
+#include <QSpinBox>
+#include <QPushButton>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSlider>
 
 class Alarm : public QWidget
 {
@@ -10,5 +17,29 @@ class Alarm : public QWidget
 public:
     Alarm(QWidget *parent = nullptr);
     ~Alarm();
+public slots:
+
+private:
+    // funtionality related variable
+    QLabel *separater;
+    QLabel *status;
+    QLabel *setVolume;
+    QPushButton *setAlarmbtn;
+    QPushButton *snoozebtn;     // snooze the alarm
+    QPushButton *stopbtn;       // stop the alarm
+    QSlider *volumebar;         // control the volume of the alarm
+    QSpinBox *hourBox;
+    QSpinBox *minuteBox;
+
+    // status variable
+    unsigned int hour, minute;
+    bool snoozed;
+    bool activated;
+
+    // layout manager
+    QHBoxLayout *timeHbox;
+    QHBoxLayout *volumeHbox;
+    QHBoxLayout *btnHbox;
+    QVBoxLayout *vbox;
 };
 #endif // ALARM_H
