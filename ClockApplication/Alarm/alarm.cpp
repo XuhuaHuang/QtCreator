dc26d160f3cb53260d9ba39de730c09c092d68cd
Huang <xuhuahuang0412@gmail.com>
2021-03-26 14:34:47 -0400

    Committed

--- ClockApplication/Alarm/alarm.cpp
+++ ClockApplication/Alarm/alarm.cpp
@@ -5,13 +5,33 @@ Alarm::Alarm(QWidget *parent)
       hour(0),
       minute(0),
       snoozed(false),
-      activated(false)
+      activated(false),
+      alarmVolume(0)
 {
     // initialize layout
     setWindowTitle("Alarm");
     // change the background color to dark grey
     setStyleSheet("background-color:#848484;");
     resize(280, 120); // set fixed-size window
+    drawApp();        // call function to draw the app
+
+    currentTime = QTime::currentTime();
+
+    connect(setAlarmbtn, SIGNAL(clicked()), this, SLOT(setAlarmTime()));
+    connect(snoozebtn, SIGNAL(clicked()), this, SLOT(snoozeAlarm()));
+    connect(stopbtn, SIGNAL(clicked()), this, SLOT(stopAlarm()));
+
+    connect(volumebar, SIGNAL(valueChanged(int)), this, SLOT(setAlarmVolume(int)));
+    connect(hourBox, SIGNAL((valueChanged(int))), this, SLOT(setHour(int)));
+    connect(minuteBox, SIGNAL(valueChanged(int)), this, SLOT(setMinute(int)));
+}
+
+Alarm::~Alarm()
+{
+}
+
+void Alarm::drawApp()
+{
     timeHbox = new QHBoxLayout();
     volumeHbox = new QHBoxLayout();
     btnHbox = new QHBoxLayout();
@@ -21,7 +41,7 @@ Alarm::Alarm(QWidget *parent)
     separater = new QLabel(":");
     separater->setAlignment(Qt::AlignHCenter);
     status = new QLabel("Alarm status");
-    setVolume = new QLabel("Set Volume");
+    volume = new QLabel("Set Volume");
     setAlarmbtn = new QPushButton("Set Alarm");
     snoozebtn = new QPushButton("Snooze Alarm");
     stopbtn = new QPushButton("Stop Alarm");
@@ -30,10 +50,12 @@ Alarm::Alarm(QWidget *parent)
     volumebar->setMinimum(0);
     volumebar->setMaximum(100);
 
+    // hour spin box is limited from 0 to 23
     hourBox = new QSpinBox();
     hourBox->setMinimum(0);
     hourBox->setMaximum(23);
 
+    // minute spin box is limited from 0 to 59
     minuteBox = new QSpinBox();
     minuteBox->setMinimum(0);
     minuteBox->setMaximum(59);
@@ -44,7 +66,7 @@ Alarm::Alarm(QWidget *parent)
     timeHbox->addWidget(minuteBox);
     timeHbox->addWidget(setAlarmbtn);
 
-    volumeHbox->addWidget(setVolume);
+    volumeHbox->addWidget(volume);
     volumeHbox->addWidget(volumebar);
 
     btnHbox->addWidget(snoozebtn);
@@ -57,6 +79,20 @@ Alarm::Alarm(QWidget *parent)
     setLayout(vbox);
 }
 
-Alarm::~Alarm()
+void Alarm::setAlarmTime()
 {
+    alarmTime.setHMS(hour, minute, 0);
+    if (activated)
+    {
+        soundAlarm(alarmTime);
+    }
+
+}
+
+void Alarm::soundAlarm(const QTime& alarmTime)
+{
+    if (!snoozed && volume != 0)
+    {
+
+    }
 }
--- ClockApplication/Alarm/alarm.h
+++ ClockApplication/Alarm/alarm.h
@@ -9,6 +9,8 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QSlider>
+#include <QTime>
+#include <QtMultimedia/QMediaPlayer>
 
 class Alarm : public QWidget
 {
@@ -18,12 +20,19 @@ public:
     Alarm(QWidget *parent = nullptr);
     ~Alarm();
 public slots:
+    void setAlarmTime();
+    void setHour(int userHour) { hour = userHour; }
+    void setMinute(int userMinute) { minute = userMinute; }
+    void soundAlarm(const QTime&);
 
+    void snoozeAlarm() { snoozed = true; }
+    void stopAlarm() { activated = false; }
+    void setAlarmVolume(int volume) { alarmVolume = volume; }
 private:
     // funtionality related variable
     QLabel *separater;
     QLabel *status;
-    QLabel *setVolume;
+    QLabel *volume;
     QPushButton *setAlarmbtn;
     QPushButton *snoozebtn;     // snooze the alarm
     QPushButton *stopbtn;       // stop the alarm
@@ -32,14 +41,19 @@ private:
     QSpinBox *minuteBox;
 
     // status variable
+    QTime alarmTime;
+    QTime currentTime;
     unsigned int hour, minute;
     bool snoozed;
     bool activated;
+    unsigned int alarmVolume;
 
     // layout manager
     QHBoxLayout *timeHbox;
     QHBoxLayout *volumeHbox;
     QHBoxLayout *btnHbox;
     QVBoxLayout *vbox;
+
+    void drawApp();
 };
 #endif // ALARM_H
