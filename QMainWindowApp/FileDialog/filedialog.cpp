dae0008c0e8db1928b466fc2f4c3219482e00297
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-04-12 17:41:08 -0400

    Updated project file dialog

--- QMainWindowApp/FileDialog/filedialog.cpp
+++ QMainWindowApp/FileDialog/filedialog.cpp
@@ -1,6 +1,6 @@
 /*****************************************************************//**
  * \file   filedialog.cpp
- * \brief  
+ * \brief  Contains implementation for class FileDialog
  * 
  * \author Xuhua Huang
  * \date   April 2021
@@ -11,8 +11,69 @@
 FileDialog::FileDialog(QWidget *parent)
     : QWidget(parent)
 {
+    /* INSTANCIATE PRIVATE ATTRIBUTES */
+    openFileBtn = new QPushButton("Open Text File");
+    saveFileBtn = new QPushButton("Save File");
+    plnTxtEdt = new QPlainTextEdit();
+
+    /* Layout Manager */
+    btnHbox = new QHBoxLayout();
+    vbox = new QVBoxLayout();
+
+    /* SET APPLICATION LAYOUT */
+    btnHbox->addWidget(openFileBtn); btnHbox->addWidget(saveFileBtn);
+    vbox->addLayout(btnHbox);
+    vbox->addWidget(plnTxtEdt);
+    setLayout(vbox);
+    setWindowTitle("File Dialog");
+    setWindowIcon(QIcon("./images/editor.png"));
+    resize(400, 500);
+
+    /* CONNECT SIGNALS AND SLOTS */
+    connect(openFileBtn, SIGNAL(clicked()), this, SLOT(openBtnClicked()));
+    connect(saveFileBtn, SIGNAL(clicked()), this, SLOT(saveBtnClicked()));
 }
 
-FileDialog::~FileDialog()
+FileDialog::~FileDialog() {}
+
+void FileDialog::openBtnClicked()
 {
+    qDebug() << "FileDialog::openBtnClicked() invoked" << endl;
+    // default filter
+    QString fileFilter = tr("Text Files (*.txt)");
+    // retreive the file name with path
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Open Text Files"), "C:/",
+        tr("Text Files (*.txt)"), &fileFilter);
+    // create a QFile object with known file path
+    QFile data(fileName);
+    // string to store actual content in the file
+    QString content;
+
+    if (data.open(QFile::ReadOnly)) {
+        QTextStream in(&data);
+        in.setCodec("UTF-8");
+        // clelar text edit field
+        plnTxtEdt->clear();
+        do {
+            content = in.readLine();
+            plnTxtEdt->insertPlainText(content + "\n");
+            // start a new line at the end of each read line
+        } while (!content.isNull());
+    } // end reading QFile object
+}
+
+void FileDialog::saveBtnClicked()
+{
+    qDebug() << "FileDialog::saveBtnClicked() invoked" << endl;
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Save Text File"), "C:/untitled.txt",
+        tr("Text Files (*.txt)"));
+
+    QFile outputFile(fileName);
+    if (outputFile.open(QFile::WriteOnly)) {
+        QTextStream out(stdout);
+        out.setCodec("UTF-8");
+        // while()
+    }
 }
--- QMainWindowApp/FileDialog/filedialog.h
+++ QMainWindowApp/FileDialog/filedialog.h
@@ -1,6 +1,9 @@
 /*****************************************************************//**
  * \file   filedialog.h
- * \brief  
+ * \brief  Create a simple QWidget application to open, edit and save
+ *  a .txt file with
+ *  QFileDialog::getOpenFileName()
+ *  and QFileDialog::getSaveFileName() methods
  * 
  * \author Xuhua Huang
  * \date   April 2021
@@ -10,6 +13,12 @@
 #define FILEDIALOG_H
 
 #include <QWidget>
+#include <QFileDialog>
+#include <QPushButton>
+#include <QPlainTextEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QDebug>
 
 class FileDialog : public QWidget
 {
@@ -18,5 +27,16 @@ class FileDialog : public QWidget
 public:
     FileDialog(QWidget *parent = nullptr);
     ~FileDialog();
+public slots:
+    void openBtnClicked(void);
+    void saveBtnClicked(void);
+private:
+    // button to open and save a file
+    QPushButton *openFileBtn, *saveFileBtn;
+    // plain text editing field
+    QPlainTextEdit *plnTxtEdt;
+
+    QVBoxLayout *vbox;
+    QHBoxLayout *btnHbox;
 };
 #endif // FILEDIALOG_H
