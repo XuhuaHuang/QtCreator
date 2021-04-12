8a6ad136ee00fbb28570c5f941f09f1e5902ce92
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2021-04-12 14:54:23 -0400

    Create filedialog.cpp

---
+++ QMainWindowApp/FileDialog/filedialog.cpp
@@ -0,0 +1,18 @@
+/*****************************************************************//**
+ * \file   filedialog.cpp
+ * \brief  
+ * 
+ * \author Xuhua Huang
+ * \date   April 2021
+ *********************************************************************/
+
+#include "filedialog.h"
+
+FileDialog::FileDialog(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+FileDialog::~FileDialog()
+{
+}
