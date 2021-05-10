4c69fc0e7cc335bef4991f8664276dba1e699b96
XuhuaHuang <71360506+XuhuaHuang@users.noreply.github.com>
2021-05-10 10:37:54 -0400

    Create sqrtcalc.h

---
+++ FinalExam/SquareRootCalc/sqrtcalc.h
@@ -0,0 +1,14 @@
+#ifndef SQRTCALC_H
+#define SQRTCALC_H
+
+#include <QWidget>
+
+class SqrtCalc : public QWidget
+{
+    Q_OBJECT
+
+public:
+    SqrtCalc(QWidget *parent = nullptr);
+    ~SqrtCalc();
+};
+#endif // SQRTCALC_H
