7913ccc6d1f44eb3c94a355ca609029704ab0191
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-05-10 23:09:59 -0400

    coming: handle button pressed event

--- FinalExam/UserInfoWidget/userinfo.cpp
+++ FinalExam/UserInfoWidget/userinfo.cpp
@@ -3,9 +3,76 @@
 UserInfo::UserInfo(QWidget *parent)
     : QWidget(parent)
 {
+    initElements();
+    setAppLayout();
 }
 
 UserInfo::~UserInfo()
 {
 }
 
+void UserInfo::initElements()
+{
+    firstNamelbl = new QLabel("First Name:");
+    lastNamelbl = new QLabel("Last Name:");
+    stAddrlbl = new QLabel("Street Address:");
+    citylbl = new QLabel("City:");
+
+    firstNameedt = new QLineEdit();
+    lastNameedt = new QLineEdit();
+    stAddredt = new QLineEdit();
+    cityedt = new QLineEdit();
+
+    provlbl = new QLabel("Province");
+    provcbb = new QComboBox();
+    provcbb->addItems(provStrList);
+
+    postlbl = new QLabel("Postal Code");
+    postedt = new QLineEdit();
+    postalcoderegexp = new QRegExpValidator(QRegExp("[A-Z]{1}[0-9]{1}[A-Z]{1}" // char-int-char
+                                                    "[0-9]{1}[A-Z]{1}[0-9]{1}")); // int-char-int
+    postedt->setValidator(postalcoderegexp);
+
+    readOnlyRst = new QTextEdit();
+
+    okbtn = new QPushButton("OK");
+    quitbtn = new QPushButton("Quit");
+}
+
+void UserInfo::setAppLayout()
+{
+    lblEdtLayout = new QGridLayout();
+    lblEdtLayout->addWidget(firstNamelbl, 0, 0);
+    lblEdtLayout->addWidget(firstNameedt, 0, 1);
+    lblEdtLayout->addWidget(lastNamelbl, 1, 0);
+    lblEdtLayout->addWidget(lastNameedt, 1, 1);
+    lblEdtLayout->addWidget(stAddrlbl, 2, 0);
+    lblEdtLayout->addWidget(stAddredt, 2, 1);
+    lblEdtLayout->addWidget(citylbl, 3, 0);
+    lblEdtLayout->addWidget(cityedt, 3, 1);
+
+    provPsthbox = new QHBoxLayout();
+    provPsthbox->addWidget(provlbl);
+    provPsthbox->addWidget(provcbb);
+    provPsthbox->addWidget(postlbl);
+    provPsthbox->addWidget(postedt);
+
+    btnhbox = new QHBoxLayout();
+    btnhbox->addWidget(okbtn);
+    btnhbox->addWidget(quitbtn);
+
+    mainLayout = new QVBoxLayout();
+    mainLayout->addLayout(lblEdtLayout);
+    mainLayout->addLayout(provPsthbox);
+    mainLayout->addLayout(btnhbox);
+    mainLayout->addWidget(readOnlyRst);
+
+    this->setLayout(mainLayout);
+    this->setWindowTitle("User Information Widget");
+    this->setFixedSize(400, 450);
+}
+
+void UserInfo::okbtnClicked()
+{
+
+}
--- FinalExam/UserInfoWidget/userinfo.h
+++ FinalExam/UserInfoWidget/userinfo.h
@@ -2,6 +2,15 @@
 #define USERINFO_H
 
 #include <QWidget>
+#include <QLabel>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QRegExpValidator>
+#include <QComboBox>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QGridLayout>
 
 class UserInfo : public QWidget
 {
@@ -10,5 +19,29 @@ class UserInfo : public QWidget
 public:
     UserInfo(QWidget *parent = nullptr);
     ~UserInfo();
+
+public slots:
+    void okbtnClicked();
+
+private:
+    void initElements();
+    void setAppLayout();
+
+    QLabel *firstNamelbl, *lastNamelbl, *stAddrlbl, *citylbl, *provlbl, *postlbl;
+    QLineEdit *firstNameedt, *lastNameedt, *stAddredt, *cityedt, *postedt;
+
+    QComboBox *provcbb;
+    const QStringList provStrList = {
+        "AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "SK", "YT"
+    };
+    QRegExpValidator *postalcoderegexp;
+
+    QTextEdit *readOnlyRst;
+    QPushButton *okbtn, *quitbtn;
+
+    QGridLayout *lblEdtLayout;
+    QHBoxLayout *provPsthbox;
+    QHBoxLayout *btnhbox;
+    QVBoxLayout *mainLayout;
 };
 #endif // USERINFO_H
